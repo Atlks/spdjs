83a1648d0d10bf60e1541fcf8e858ff51148cfa0
ADAFD <EM>
2020-03-20 00:05:52 +0800

    app statd

--- url2rssGoogle.js
+++ url2rssGoogle.js
@@ -1,20 +1,20 @@
 (async () => {
 
-mod = require('./flwUrl2rss.js');
-rzt =await mod.feilonveo2rss('https://www.flw.ph/forum-40-1.html','thread');
-console.log( rzt  );
- 
+    mod = require('./flwUrl2rss.js');
+    rzt = await mod.feilonveo2rss('https://www.flw.ph/forum-40-1.html', 'thread');
+    console.log(rzt);
+
 })();
 
-return;
+//return;
 // //throw 5;
 
 
 // (async () => {
 
-        
 
-  
+
+
 // })();
 
 
@@ -22,18 +22,21 @@ var http = require('http');
 
 // create a server object:
 http.createServer(function (req, res) {
-   // logger = getLogger();
+    // logger = getLogger();
     (async () => {
 
-        mod = require('./flwUrl2rss.js');
-        message = await mod.feilonveo2rss();
-     //   res.status(200).send(message);
-         res.write(message); //write a response to the client
-        res.end();
-      
+        var url = require('url');
+        if (req.url == '/flw') {
+
+            mod = require('./flwUrl2rss.js');
+            message = await mod.feilonveo2rss('https://www.flw.ph/forum-40-1.html', 'thread');
+            //   res.status(200).send(message);
+            res.write(message); //write a response to the client
+            res.end();
+        }
+
     })();
 
-}).listen(888); //the server object listens on port 8080
+}).listen(80); //the server object listens on port 8080
 
 console.log("f");
- 
